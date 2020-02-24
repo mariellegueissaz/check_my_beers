a076c049b8c19c526c366af5af7941e86ca9772b
Marielle Gueissaz <marielle.gueissaz@gmail.com>
2020-02-24 17:32:50 +0100

    created show without CSS

--- app/controllers/beers_controller.rb
+++ app/controllers/beers_controller.rb
@@ -3,6 +3,12 @@ class BeersController < ApplicationController
   end
 
   def show
+    @beer = Beer.find(params[:id])
+     if @beer.photo.attached?
+      @photo = @beer.photo
+    else
+      @photo = "https://static.mycity.travel/manage/uploads/7/40/117655/1/biere_3000.jpg"
+    end
   end
 
   def scan
