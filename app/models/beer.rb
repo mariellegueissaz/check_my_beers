46624ae92977cd1797df23694f0fa49f15c0a977
Arthur Messean <arthur.messean@gmail.com>
2020-02-27 14:13:09 +0100

    favorite done

--- app/models/beer.rb
+++ app/models/beer.rb
@@ -2,6 +2,7 @@ require 'csv'
 
 class Beer < ApplicationRecord
   has_many :reviews, dependent: :destroy
+  has_many :favorites, dependent: :destroy
   has_one_attached :photo
   csv_options = {col_sep: ';', quote_char: '|', headers: :first_row }
   filepath    = 'app/assets/csv/beers.csv'
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_02_24_160010) do
+ActiveRecord::Schema.define(version: 2020_02_27_094456) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -51,6 +51,15 @@ ActiveRecord::Schema.define(version: 2020_02_24_160010) do
     t.string "brewery"
   end
 
+  create_table "favorites", force: :cascade do |t|
+    t.bigint "beer_id"
+    t.bigint "user_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["beer_id"], name: "index_favorites_on_beer_id"
+    t.index ["user_id"], name: "index_favorites_on_user_id"
+  end
+
   create_table "reviews", force: :cascade do |t|
     t.text "content"
     t.integer "rate"
@@ -75,6 +84,8 @@ ActiveRecord::Schema.define(version: 2020_02_24_160010) do
   end
 
   add_foreign_key "active_storage_attachments", "active_storage_blobs", column: "blob_id"
+  add_foreign_key "favorites", "beers"
+  add_foreign_key "favorites", "users"
   add_foreign_key "reviews", "beers"
   add_foreign_key "reviews", "users"
 end
