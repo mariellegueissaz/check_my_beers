a0f6fc748163a1e8e243e9baa320ae8f112b3df3
Lauriane Bonnet <laurianebonnet91@gmail.com>
2020-03-02 09:31:45 +0100

    advanced search hide/show

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -3,8 +3,11 @@ import * as ScanditSDK from "scandit-sdk";
 import 'mapbox-gl/dist/mapbox-gl.css'; // <-- you need to uncomment the stylesheet_pack_tag in the layout!
 
 import { initMapbox } from '../plugins/init_mapbox';
+import { openAdvancedSearch } from '../components/nav_search';
 
 initMapbox();
+openAdvancedSearch();
+
 
 ScanditSDK.configure("AQN+4R24A/5tO11u+j0nZ1AecF3/Cv/d3H4uZrsNjqZXX9qQqWGKJu5+LKLNMybTtEhS7TMZEHsPV3MOg00NqcVBz9gYb9qirFjq42YtyKOTQeTkfmBz4jNbCb91R5AZChQ1/3lhl9PBRI/25lG6J6NZ3SQ2d9zPiXcjvVFw7z8PVZn6eRl5kNpEqoCuJs5fik0USP5WSnb02nCQP9qS1salnhfRMQPdbB66jXOpyQZG80JyHwTX4NPQjAJqVM4Y2cRNE8xUMWjeZGkNcl+kZOXvidtxMr/CjaPhCvnhgQvj6QD6xX15B9csIecI5baFC7/9WIXh8PQg+sxBODv7NcL0dCDN0MOumXmOvznTbjAb05T0JVqGlvVW3OsIE9t6G0PM8C2FjArfc7jKN+uB7td6dbmPHeZtBOv6cvk5KTZNh7974e0CUgzdLoECMuQ7hfYVDXFWv/8m7A3Ii7YGzXcEwtrOlcHjawVYrvnXCad30b0TXFC9S7s+u0aRVhoL3h1NbWvJPvM6JQDLvTtM3kUtZ1fII2AQfxEoDms+1RMCfJrVc5JUWdmh3hSa5eC1S2NyUWnU4RTrWHa3Zx30T3Jfm9gcOoKiSeinMaRD4pwZ45wA0aHTK+3okSEe6kT4RHMobyVeMpMgOhozgXGITddYcTQqLyvtP61mPaDlsAFHeIvlvJl3z8nnQnRhMXyt3Ij/+6vUrlchr7r6H46B/X/fVdcHvK6T0oGTov8z2bmQ07f7ZQlSF75flNutywg6pRz/r8ciCDcGHt2tTLld070ExxVxQ+bNKBau1iK/WREzAzCNJpDGosshg6dAHCZK68vJgeBNsEovpBtSK5OdJYKrTTuDwfyahXYb4alFbg==", {
   engineLocation: "https://unpkg.com/scandit-sdk@4.x/build"
