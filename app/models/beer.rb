105d254cf69b3980154590ecff1c81b82a270cc8
Marielle Gueissaz <marielle.gueissaz@gmail.com>
2020-03-04 11:39:06 +0100

    bugfixes for map, popup on map, and database

--- app/models/beer.rb
+++ app/models/beer.rb
@@ -4,8 +4,8 @@ class Beer < ApplicationRecord
   has_many :reviews, dependent: :destroy
   has_many :favorites, dependent: :destroy
   has_one_attached :photo
-  geocoded_by :location
-  after_validation :geocode, if: :will_save_change_to_location?
+  geocoded_by :address
+  after_validation :geocode, if: :will_save_change_to_address?
   csv_options = {col_sep: ';', quote_char: '|', headers: :first_row }
   filepath    = 'app/assets/csv/beers.csv'
   LOCATION = []
@@ -13,9 +13,9 @@ class Beer < ApplicationRecord
   MAIN = []
   BREWERY = []
   CSV.foreach(filepath, csv_options) do |row|
-  LOCATION << row['manufacturing_places']
-  CATEGORIES << row['categories']
-  MAIN << row['main_category']
-  BREWERY << row['brands']
-end
+    LOCATION << row['manufacturing_places'] unless row['manufacturing_places'].nil?
+    CATEGORIES << row['categories'] unless row['categories'].nil?
+    MAIN << row['main_category'] unless row['main_category'].nil?
+    BREWERY << row['brands'] unless row['brands'].nil?
+  end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_03_03_130255) do
+ActiveRecord::Schema.define(version: 2020_03_04_082453) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -52,6 +52,7 @@ ActiveRecord::Schema.define(version: 2020_03_03_130255) do
     t.float "latitude"
     t.float "longitude"
     t.string "picture_url"
+    t.string "address"
   end
 
   create_table "favorites", force: :cascade do |t|
