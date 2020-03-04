8b2f3790813ae4d28aebf15e53c4831ce97e20b1
Arthur Messean <arthur.messean@gmail.com>
2020-03-04 10:37:51 +0100

    community

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,5 +2,6 @@ class UsersController < ApplicationController
 
   def show
     @user = User.find(params[:id])
+    @marker = [ { lat: @user.latitude, lng: @user.longitude, infoWindow: render_to_string(partial: "info_window", locals: { beer: @beer }), image_url: helpers.asset_url('beer_pin.png') } ]
   end
 end
